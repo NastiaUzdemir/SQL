f9162105a02b8a99d12facf4996756d2c0e196b8
Neto test <uzdemir.a@yandex.ru>
2024-06-14 17:08:52 +0400

    first commit

---
+++ src/test/java/ru/netology/data/SQLHelper.java
@@ -0,0 +1,44 @@
+package ru.netology.data;
+import lombok.SneakyThrows;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
+
+public class SQLHelper {
+    private static QueryRunner QUERY_RUNNER = new QueryRunner();
+
+    private SQLHelper() {
+    }
+
+    //Подключение к БД
+    private static Connection getConn() throws SQLException {
+        return DriverManager.getConnection(System.getProperty("db.url"), "app", "pass");
+    }
+
+    //получить код верификации
+    @SneakyThrows
+    public static DataHelper.VerificationCode getVerificationCode() {
+        var codeSQL = "SELECT code FROM auth_codes ORDER BY created DESC LIMIT 1";
+        var conn = getConn();
+        return QUERY_RUNNER.query(conn, codeSQL, new BeanHandler<>(DataHelper.VerificationCode.class));
+    }
+
+    //Очищает БД
+    @SneakyThrows
+    public static void cleanDatabase() {
+        var connection = getConn();
+        QUERY_RUNNER.execute(connection, "DELETE FROM auth_codes");
+        QUERY_RUNNER.execute(connection, "DELETE FROM card_transactions");
+        QUERY_RUNNER.execute(connection, "DELETE FROM cards");
+        QUERY_RUNNER.execute(connection, "DELETE FROM users");
+    }
+
+    //Очищает таблицу auth_codes
+    @SneakyThrows
+    public static void cleanAuthCodes() {
+        var connection = getConn();
+        QUERY_RUNNER.execute(connection, "DELETE FROM auth_codes");
+    }
+}
